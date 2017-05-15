ddb672a12229e7f21d16c3d6697fa59211b335a5
Linus Miller <lohfu@lohfu.io>
2017-05-15 14:08:14 +0200

    next-all: Use dom-is instead of Element.matches

--- packages/next-all/src/index.js
+++ packages/next-all/src/index.js
@@ -1,10 +1,12 @@
+import is from 'dom-is';
+
 export default function nextAll(element, selector) {
   const siblings = Array.from(element.parentNode.children);
 
   let next = siblings.slice(siblings.indexOf(element) + 1);
 
   if (selector) {
-    next = next.filter((el) => el.matches(selector));
+    next = next.filter((el) => is(el, selector));
   }
 
   return next;

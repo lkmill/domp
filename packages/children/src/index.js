8d8587f4e170434adfcc3f4bbf33ec6e16a8a69e
Linus Miller <lohfu@lohfu.io>
2017-05-15 14:39:11 +0200

    children: Initial commit

---
+++ packages/children/src/index.js
@@ -0,0 +1,19 @@
+import is from 'dom-is';
+
+/**
+ * @param {Element} element - element to get children of
+ * @param {string|number|Node|Node[]|NodeList|HTMLCollection} criteria -
+ * Criteria to test against (see dom-is)
+ * @returns {Element[]} Array of all (matching) children
+ */
+export default function domChildren(element, criteria) {
+  if (!element || !element.children) return [];
+
+  let children = Array.from(element.children);
+
+  if (criteria) {
+    children = children.filter((child) => is(child, criteria));
+  }
+
+  return children;
+}

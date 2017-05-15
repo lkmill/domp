6aae956146b87aeca070c971d8406fe214df1730
Linus Miller <lohfu@lohfu.io>
2017-05-15 14:40:09 +0200

    siblings: Initial commit

---
+++ packages/siblings/README.md
@@ -0,0 +1,2 @@
+# dom-siblings
+Select all siblings to a DOM node matching an optional criteria
---
+++ packages/siblings/src/index.js
@@ -0,0 +1,9 @@
+import is from 'dom-is';
+
+export default function siblings(element, selector) {
+  const children = Array.from(element.parentNode.children);
+
+  return selector
+    ? children.filter((el) => el !== element && is(el, selector))
+    : [...children.slice(0, index), ...children.slice(index + 1)];
+}

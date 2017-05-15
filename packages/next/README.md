7898ed8a26c7c83d3a07c08d83f903e5cddaa0b9
Linus Miller <lohfu@lohfu.io>
2017-05-15 11:29:58 +0200

    next: Initial commit

---
+++ packages/next/README.md
@@ -0,0 +1,2 @@
+# dom-next
+Select the next DOM node matching an (optional) criteria
---
+++ packages/next/src/index.js
@@ -0,0 +1,11 @@
+import is from 'dom-is';
+
+export default function next(element, selector) {
+  let nextElement = element.nextElementSibling || null;
+
+  if (nextElement && selector && !is(nextElement, selector)) {
+    nextElement = null;
+  }
+
+  return nextElement;
+}

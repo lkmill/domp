283796d2d7cf2fdba8fc48960e8c11bc9fc4ee38
Linus Miller <lohfu@lohfu.io>
2018-03-01 19:56:51 +0100

    is: Fix code to pass new eslint config

--- packages/is/src/index.mjs
+++ packages/is/src/index.mjs
@@ -9,16 +9,16 @@ if (!Element.prototype.matches) {
     Element.prototype.oMatchesSelector ||
     Element.prototype.webkitMatchesSelector ||
     function (s) {
-      const matches = (this.parentNode || this.document || this.ownerDocument).querySelectorAll(s);
+      const matches = (this.parentNode || this.document || this.ownerDocument).querySelectorAll(s)
 
       for (let i = 0; i < matches.length; i++) {
         if (matches[i] === this) {
-          return true;
+          return true
         }
       }
 
-      return false;
-    };
+      return false
+    }
 }
 
 /**
@@ -26,23 +26,23 @@ if (!Element.prototype.matches) {
  * @param {string|number|Node|Node[]|NodeList|HTMLCollection} criteria - Criteria to test against
  * @returns {boolean} - Whether Node passes criteria
  */
-export default function is(node, criteria) {
+export default function is (node, criteria) {
   if (!criteria) {
-    return false;
+    return false
   } else if (typeof criteria === 'string') {
-    return node instanceof Element && node.matches(criteria);
+    return node instanceof Element && node.matches(criteria)
   } else if (criteria instanceof Node) {
-    return node === criteria;
+    return node === criteria
   } else if (typeof criteria === 'number') {
-    return node.nodeType === criteria;
+    return node.nodeType === criteria
   } else if (Array.isArray(criteria)) {
     // TODO maybe call `is` recursively here so one can
     // pass an array of selectors. or an array of
     // different types ie `criteria.some((item) => is(node, item))`
-    return criteria.indexOf(node) > -1;
+    return criteria.indexOf(node) > -1
   } else if (criteria.length && typeof criteria !== 'function') {
-    return Array.from(criteria).indexOf(node) > -1;
+    return Array.from(criteria).indexOf(node) > -1
   }
 
-  return false;
+  return false
 }

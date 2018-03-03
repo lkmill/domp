3ed48e927a713bf16fc011ffb7eb564d3fe348c6
Linus Miller <lohfu@lohfu.io>
2018-03-03 14:26:03 +0100

    create: Eslint --fix

--- packages/create/src/index.mjs
+++ packages/create/src/index.mjs
@@ -1,8 +1,8 @@
-export default function createOne(htmlString) {
+export default function createOne (htmlString) {
   // TODO either format htmlString or make sure no empty text nodes have been created
-  const container = document.createElement('div');
+  const container = document.createElement('div')
 
-  container.innerHTML = htmlString;
+  container.innerHTML = htmlString
 
-  return container.childNodes[0];
+  return container.childNodes[0]
 }

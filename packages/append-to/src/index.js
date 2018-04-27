034c5775c90ac981048e9054e975330bd7a0c325
Linus Miller <lohfu@lohfu.io>
2018-04-27 15:30:15 +0200

    append-to: Eslint --fix

--- packages/append-to/src/index.js
+++ packages/append-to/src/index.js
@@ -1,16 +1,16 @@
 // TODO implement appendTo(nodeList, nodeList)
-export default function appendTo(element, reference) {
+export default function appendTo (element, reference) {
   if (reference.parentNode) {
     if (element instanceof Node) {
-      reference.appendChild(element);
+      reference.appendChild(element)
     } else {
       if (element instanceof NodeList || element instanceof HTMLCollection) {
-        element = Array.from(element);
+        element = Array.from(element)
       }
 
       element.forEach((el) => {
-        reference.appendChild(el);
-      });
+        reference.appendChild(el)
+      })
     }
   }
 }

55334ac170b76536994086f52d8cacb56a28e518
Linus Miller <lohfu@lohfu.io>
2018-05-06 17:27:00 +0200

    replace-with: Set up basic tests

---
+++ packages/replace-with/__tests__/index.mjs
@@ -0,0 +1,19 @@
+import replaceWith from '../src/index'
+
+const html = `
+<div id='a-1' class='a a-1'></div>
+`
+
+document.body.innerHTML = html
+
+const a1 = document.getElementById('a-1')
+
+test('basic test', () => {
+  expect(document.body.firstElementChild).toBe(a1)
+
+  const el = document.createElement('span')
+
+  replaceWith(a1, el)
+
+  expect(document.body.firstElementChild).toBe(el)
+})

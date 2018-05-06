bb9120332d99241ee7c63b2cde2592448b05537b
Linus Miller <lohfu@lohfu.io>
2018-05-06 17:22:26 +0200

    replace-with: Update build process to use adjacent .js and .mjs file and output UMD build

---
+++ packages/replace-with/rollup.config.js
@@ -0,0 +1,14 @@
+import babel from 'rollup-plugin-babel'
+
+export default {
+  plugins: [
+    babel({
+      exclude: 'node_modules/**/*',
+    }),
+  ],
+  input: 'src/index.mjs',
+  output: {
+    format: 'umd',
+    name: '$replaceWith',
+  },
+}

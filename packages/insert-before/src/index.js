904dd7f897da4919fed6a4813a634765703c2592
Linus Miller <lohfu@lohfu.io>
2018-05-06 12:11:05 +0200

    insert-before: Switch to standard eslint and Eslint --fix

--- packages/insert-before/src/index.js
+++ packages/insert-before/src/index.js
@@ -2,15 +2,15 @@
 export default function (element, reference) {
   if (reference.parentNode) {
     if (element instanceof Node) {
-      reference.parentNode.insertBefore(element, reference);
+      reference.parentNode.insertBefore(element, reference)
     } else {
       if (element instanceof NodeList || element instanceof HTMLCollection) {
-        element = Array.from(element);
+        element = Array.from(element)
       }
 
       element.forEach((el) => {
-        reference.parentNode.insertBefore(el, reference);
-      });
+        reference.parentNode.insertBefore(el, reference)
+      })
     }
   }
 }

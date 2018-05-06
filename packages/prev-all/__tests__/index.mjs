452490d98dc1e934c6fd695abbe6a43965d87ba4
Linus Miller <lohfu@lohfu.io>
2018-05-06 17:06:08 +0200

    prev-all: Set up basic tests

---
+++ packages/prev-all/__tests__/index.mjs
@@ -0,0 +1,43 @@
+import prevAll from '../src/index'
+
+const html = `
+<div id='a-1' class='a a-1'></div>
+<div id='a-2' class='a a-2'></div>
+<div id='a-3' class='a a-3'></div>
+<div id='a-4' class='a a-4'></div>
+<div id='a-5' class='a a-5'></div>
+`
+
+document.body.innerHTML = html
+
+const a1 = document.getElementById('a-1')
+const a2 = document.getElementById('a-2')
+const a3 = document.getElementById('a-3')
+const a4 = document.getElementById('a-4')
+const a5 = document.getElementById('a-5')
+
+test('basic test', () => {
+  let result = prevAll(a5)
+
+  expect(result).toEqual([ a1, a2, a3, a4 ])
+
+  result = prevAll(a4, '.a')
+
+  expect(result).toEqual([ a1, a2, a3 ])
+
+  result = prevAll(a4, '.a-2')
+
+  expect(result).toEqual([ a2 ])
+
+  result = prevAll(a5, '#a-2')
+
+  expect(result).toEqual([ a2 ])
+
+  result = prevAll(a2, '#x')
+
+  expect(result).toEqual([])
+
+  result = prevAll(a1)
+
+  expect(result).toEqual([])
+})

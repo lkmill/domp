e8bc30da0523effa3b88812dc4a82bf489c5c213
Linus Miller <lohfu@lohfu.io>
2018-05-06 18:41:09 +0200

    scroll-into-view: Update build process to use adjacent .js and .mjs file and output UMD build

---
+++ packages/scroll-into-view/src/index.mjs
@@ -0,0 +1,11 @@
+import $ from '@domp/select'
+import offset from '@domp/offset'
+import scrollTo from '@domp/scroll-to'
+
+export default function scrollIntoView (el, duration = 0, relative = 0) {
+  if (typeof el === 'string') el = $(el)
+
+  const coordY = offset(el).top - relative
+
+  scrollTo(0, coordY, duration)
+}

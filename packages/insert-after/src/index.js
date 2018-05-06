93265978b58b6cb6337bcb66447e5fb30daade34
Linus Miller <lohfu@lohfu.io>
2018-05-06 11:46:06 +0200

    insert-after: Switch to standard eslint and Eslint --fix

--- packages/insert-after/src/index.js
+++ packages/insert-after/src/index.js
@@ -1,21 +1,21 @@
 // TODO implement insertAfter(nodeList, nodeList)
 export default function (element, reference) {
   if (reference.parentNode) {
-    const next = reference.nextSibling;
+    const next = reference.nextSibling
 
     if (element instanceof Node) {
-      if (next) reference.parentNode.insertBefore(element, next);
-      else reference.parentNode.appendChild(element);
+      if (next) reference.parentNode.insertBefore(element, next)
+      else reference.parentNode.appendChild(element)
     } else {
       if (element instanceof NodeList || element instanceof HTMLCollection) {
-        element = Array.from(element);
+        element = Array.from(element)
       }
 
       element.forEach(next ? (el) => {
-        reference.parentNode.insertBefore(el, next);
+        reference.parentNode.insertBefore(el, next)
       } : (el) => {
-        reference.parentNode.appendChild(el);
-      });
+        reference.parentNode.appendChild(el)
+      })
     }
   }
 }

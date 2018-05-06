eeddc1a1d81172efcc840109b4f01041dd2eadb9
Linus Miller <lohfu@lohfu.io>
2018-05-06 14:50:02 +0200

    offset: Switch to standard eslint and eslint --fix

--- packages/offset/src/index.js
+++ packages/offset/src/index.js
@@ -1,17 +1,17 @@
-export default function offset(el) {
+export default function offset (el) {
   const result = {
     top: 0,
     left: 0,
-  };
+  }
 
-  let ref = el;
+  let ref = el
 
   // in IE offsetParent is null when position: fixed
   while (ref && ref !== document.body) {
-    result.top += ref.offsetTop;
-    result.left += ref.offsetLeft;
-    ref = ref.offsetParent;
+    result.top += ref.offsetTop
+    result.left += ref.offsetLeft
+    ref = ref.offsetParent
   }
 
-  return result;
+  return result
 }

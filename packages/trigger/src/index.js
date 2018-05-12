8a858ed0226d4367abd26ad387a3cf582c245bc8
Linus Miller <lohfu@lohfu.io>
2018-05-12 11:48:16 +0200

    trigger: Switch to standard eslint and eslint --fix

--- packages/trigger/src/index.js
+++ packages/trigger/src/index.js
@@ -1,7 +1,7 @@
-export default function trigger(element, str) {
+export default function trigger (element, str) {
   element.dispatchEvent(new Event(str, {
     view: window,
     bubbles: true,
     cancelable: true,
-  }));
+  }))
 }

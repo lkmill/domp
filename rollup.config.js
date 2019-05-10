3a97ceae2436c7a04f49ed8aa004a14777cdd8eb
Linus Miller <lohfu@lohfu.io>
2019-05-10 15:10:34 +0200

    Only build fp umd if fp.mjs exists

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const fs = require('fs')
 const _ = require('lodash')
 const babel = require('rollup-plugin-babel')
 const nodeResolve = require('rollup-plugin-node-resolve')
@@ -9,6 +10,7 @@ const babelConfig = require('./babel.config')
 const pkg = require(path.join(process.cwd(), 'package.json'))
 
 const [ , name ] = pkg.name.match(/^@domp\/(.*)$/)
+const umdGlobal = _.camelCase(name)
 
 const plugins = [
   babel({
@@ -18,22 +20,28 @@ const plugins = [
   nodeResolve(),
 ]
 
-module.exports = [{
-  plugins: plugins,
+const configs = [{
+  plugins,
   input: 'src/index.mjs',
   output: {
     file: `dist/${name}.js`,
     format: 'umd',
-    name: `$${_.camelCase(name)}`,
-    sourcemap: true,
-  },
-}, {
-  plugins: plugins,
-  input: 'src/fp.mjs',
-  output: {
-    file: `dist/${name}.fp.js`,
-    format: 'umd',
-    name: `$${_.camelCase(name)}Fp`,
+    name: `$${umdGlobal}`,
     sourcemap: true,
   },
 }]
+
+if (fs.existsSync(path.join(process.cwd(), 'src/fp.mjs'))) {
+  configs.push({
+    plugins,
+    input: 'src/fp.mjs',
+    output: {
+      file: `dist/${name}.fp.js`,
+      format: 'umd',
+      name: `$${umdGlobal}Fp`,
+      sourcemap: true,
+    },
+  })
+}
+
+module.exports = configs

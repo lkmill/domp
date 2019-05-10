86aa5b221c3d2228c97d680fed35b789190969d3
Linus Miller <lohfu@lohfu.io>
2019-05-10 10:31:06 +0200

    closest: Assimilate into monorepo
    - Set up jest testing in root

--- packages/closest/rollup.config.minify.js
+++
@@ -1,9 +0,0 @@
-import uglify from 'rollup-plugin-uglify'
-import configs from './rollup.config'
-
-configs.forEach((config) => {
-  config.output.file = config.output.file.replace(/js$/, 'min.js')
-  config.plugins = config.plugins.concat(uglify())
-})
-
-export default configs

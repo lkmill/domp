04544762408903dbaf230661bdc90437bab19f8b
Linus Miller <linus.miller@bitmill.io>
2021-06-29 19:01:27 +0200

    fp: change name of returned curry function

--- packages/fp/src/index.js
+++ packages/fp/src/index.js
@@ -2,12 +2,14 @@
  * @param {Function} fnc
  * @returns {Function}
  */
-export default function curry(fnc) {
-  return function curried(...args) {
+export default function fp(fnc) {
+  function curried(...args) {
     if (args.length < fnc.length) {
       return curried.bind(null, ...args)
     }
 
     return fnc(args[args.length - 1], ...args.slice(0, -1))
   }
+
+  return Object.defineProperty(curried, 'name', { value: fnc.name + 'Fp' })
 }

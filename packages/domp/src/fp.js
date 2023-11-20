bb35f825b68adfa9f88a3aa0e49f415fa5b7b884
Linus Miller <linus.miller@bitmill.io>
2023-11-20 21:44:27 +0100

    domp: separate entry for fp exports

---
+++ packages/domp/src/fp.js
@@ -0,0 +1,35 @@
+import ancestorsFp from '@domp/ancestors/fp'
+import appendToFp from '@domp/append-to/fp'
+import childrenFp from '@domp/children/fp'
+import closestFp from '@domp/closest/fp'
+import insertAfterFp from '@domp/insert-after/fp'
+import insertBeforeFp from '@domp/insert-before/fp'
+import isFp from '@domp/is/fp'
+import nextFp from '@domp/next/fp'
+import nextAllFp from '@domp/next-all/fp'
+import prependToFp from '@domp/prepend-to/fp'
+import prevFp from '@domp/prev/fp'
+import prevAllFp from '@domp/prev-all/fp'
+import scrollIntoViewFp from '@domp/scroll-into-view/fp'
+import siblingsFp from '@domp/siblings/fp'
+import triggerFp from '@domp/trigger/fp'
+import wrapFp from '@domp/wrap/fp'
+
+export {
+  ancestorsFp,
+  appendToFp,
+  childrenFp,
+  closestFp,
+  insertAfterFp,
+  insertBeforeFp,
+  isFp,
+  nextFp,
+  nextAllFp,
+  prependToFp,
+  prevFp,
+  prevAllFp,
+  scrollIntoViewFp,
+  siblingsFp,
+  triggerFp,
+  wrapFp,
+}
